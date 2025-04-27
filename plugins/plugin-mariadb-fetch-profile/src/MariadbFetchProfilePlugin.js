ad8d81159e5d290a67fb0fe8551c84f4db269468
CJB <chris.berno@gmail.com>
2025-04-26 22:11:38 -0400

    updates to package.json & MariadbFetchProfilePlugin.js

--- plugins/plugin-mariadb-fetch-profile/src/MariadbFetchProfilePlugin.js
+++ plugins/plugin-mariadb-fetch-profile/src/MariadbFetchProfilePlugin.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FlexPlugin } from '@twilio/flex-plugin';
-
-import CustomTaskList from './components/CustomTaskList/CustomTaskList';
+import * as Flex from '@twilio/flex-ui';
 
 const PLUGIN_NAME = 'MariadbFetchProfilePlugin';
 
@@ -10,14 +9,12 @@ export default class MariadbFetchProfilePlugin extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof import('@twilio/flex-ui') }
-   */
-  async init(flex, manager) {
-    const options = { sortOrder: -1 };
-    flex.AgentDesktopView.Panel1.Content.add(<CustomTaskList key="MariadbFetchProfilePlugin-component" />, options);
+  init(flex, manager) {
+    flex.CRMContainer.defaultProps.uriCallback = (task) => {
+      const crmUrl = task?.attributes?.crm_url || 'https://connie-profiles-v01-4zuxqqz55-connie-direct.vercel.app/search';
+      console.log('Loading CRM URL:', crmUrl);
+      return crmUrl;
+    };
   }
 }
+
