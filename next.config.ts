71e86f1ca3afa3513a74cbb39d7b4fa0928e8cb2
CJB <chris.berno@gmail.com>
2025-05-02 19:10:04 -0400

    more cors fixes

--- next.config.ts
+++ next.config.ts
@@ -24,8 +24,8 @@ const nextConfig: NextConfig = {
     const isDevelopment = process.env.NODE_ENV === 'development';
 
     const cspValue = isDevelopment
-      ? "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://db.connie.technology:3000 localhost:*; frame-ancestors 'self' https://flex.twilio.com"
-      : "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://db.connie.technology:3000; frame-ancestors 'self' https://flex.twilio.com";
+      ? "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.twilio.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://db.connie.technology:3000 localhost:* https://*.twilio.com; frame-ancestors 'self' https://flex.twilio.com"
+      : "default-src 'self'; script-src 'self' 'unsafe-inline' https://*.twilio.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://db.connie.technology:3000 https://*.twilio.com; frame-ancestors 'self' https://flex.twilio.com";
 
     return [
       {
