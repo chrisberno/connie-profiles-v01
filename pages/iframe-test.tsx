0b4e18837fa75e4d3b09c9795c1657ef24bc92ef
CJB <chris.berno@gmail.com>
2025-05-12 00:30:22 -0400

    bug fixes

--- pages/iframe-test.tsx
+++ pages/iframe-test.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 
 export default function IframeTest() {
+  const [isInIframe, setIsInIframe] = useState<boolean | null>(null);
+  const [currentTime, setCurrentTime] = useState<string>('');
+
+  useEffect(() => {
+    // Only access window object in useEffect (client-side)
+    setIsInIframe(window.self !== window.top);
+    setCurrentTime(new Date().toLocaleTimeString());
+    
+    // Update time every second
+    const timer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+    
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div style={{ padding: '20px' }}>
       <h1>Iframe Test Page</h1>
       <p>If you can see this page in an iframe, embedding is working correctly!</p>
-      <p>Current time: {new Date().toLocaleTimeString()}</p>
-      <p>
-        <code>window.self !== window.top: {String(window.self !== window.top)}</code>
-        <br />
-        (This should be "true" if viewed in an iframe)
-      </p>
+      <p>Current time: {currentTime}</p>
+      {isInIframe !== null && (
+        <p>
+          <code>window.self !== window.top: {String(isInIframe)}</code>
+          <br />
+          (This should be "true" if viewed in an iframe)
+        </p>
+      )}
     </div>
   );
 }
